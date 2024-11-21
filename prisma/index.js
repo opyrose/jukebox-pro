14c5417cc21860176e85b60fdbb0660b77bd9f80
Sophia <opyrose12@yahoo.com>
2024-11-21 12:49:49 -0500

    add user register model

--- prisma/index.js
+++ prisma/index.js
@@ -1,3 +1,14 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-module.exports = prisma;
+const bcrypt = require("bcrypt");
+const { PrismaClient }= require("@prisma/client")
+
+const prisma = new PrismaClient().$extends({
+    model:{
+        user:{
+            async register(username, password){
+                const hashedPassword = await bcrypt.hash(password, 10);
+                const user = await prisma.user.create({
+                    data: { username, password: hashedPassword},
+                })
+                return user;
+            }}}
+});
