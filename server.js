c379fef7ffb066ed2ae3d9136d65ee0b887e1c8e
Sophia <opyrose12@yahoo.com>
2024-11-21 20:56:39 -0500

    added all routes and testing file

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(require("morgan")("dev"));
+app.use(express.json());
+
+app.use(require(".api/auth").router);
+app.use("/playlists", require("./api/playlists"));
+app.use("/tracks", require("./api/tracks"));
+
+app.use((req, res,next) =>{
+    next({ status : 404, message: "That endpoint doesnt exist!"});
+    })
+
+app.use((err, req, res, next) =>{
+    console.error(err);
+    res.status(err.status ?? 500);
+    res.json(err.message ?? "Sorry, something broke ! :(");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}...`)
+})
